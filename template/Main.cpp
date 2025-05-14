d6ff18e6e60a8fabd4d25b07ffb0f51a4e230583
Ezzat <khaqanezzat007@gmail.com>
2025-05-14 23:39:17 +0430

    initial commit: add main template with utility macros for iteration

---
+++ template/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define FORBASE(i,a,b,s) for(int i = (a); s>0?i<(b):i>(b);i+=(s))
+#define FOR1(e) FORBASE(i,0,(e),1)
+#define FOR2(b,e) FORBASE(i,(b),(e),1)
+#define FOR3(b,e,s) FORBASE(i,(b),(e),(s))
+#define FOR4(i,b,e,s) FORBASE((i),(b),(e),(s))
+#define GET_FOR(_1,_2,_3,_4,name,...) name
+#define FOR(...) GET_FOR(__VA_ARGS__,FOR4,FOR3,FOR2,FOR1)(__VA_ARGS__)
+#define EACH(i,t) for(auto&(i):(t))
+
+
+
+void solve(){
+
+}
+
+
+int main(){
+
+    return 0;
+}
